8d3d27fb169cfa1679c5afde33a6deba8609622d
gentlandy <63382728+gentlandy@users.noreply.github.com>
2023-11-01 21:36:07 +0700

    feat:Services; modify_drop_down_menu

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -41,13 +41,10 @@
               <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                   <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Perawatan Kucing</a>
+                      <a href="#" class="block px-4 py-2 hover:bg-secondary dark:hover:bg-gray-600 dark:hover:text-white">Cat Grooming</a>
                     </li>
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Pelatihan Kucing</a>
-                    </li>
-                    <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Kesehatan Kucing</a>
+                      <a href="#" class="block px-4 py-2 hover:bg-secondary dark:hover:bg-gray-600 dark:hover:text-white">Our Events</a>
                     </li>
                   </ul>
               </div>
